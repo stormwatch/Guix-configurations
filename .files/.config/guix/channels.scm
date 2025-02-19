9186d176014c30c3e8300a04e3a7b55d05f37596
Ezequiel Birman <ebirman77@gmail.com>
2025-02-19 12:20:02 +0000

    Update channels

--- .files/.config/guix/channels.scm
+++ .files/.config/guix/channels.scm
@@ -3,7 +3,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "f44a49d3092d509c5e09cc27ada40a8c4d99c02d")
+          "6aa6de9fc86ddc3bcce8801a029f26b8bc0d074f")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -14,7 +14,7 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "f3f3cb06b0a852f96a1f76f6168307583e6dfac5")
+          "a641396502603458c492cc8d4c804930816e4424")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
