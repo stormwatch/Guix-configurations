c251feb2ab6038684f8dd1296dff7889d6d72c3b
Ezequiel Birman <ebirman77@gmail.com>
2025-03-02 21:29:31 +0000

    Add RDE channel

--- .files/.config/guix/base-channels.scm
+++ .files/.config/guix/base-channels.scm
@@ -1,4 +1,13 @@
-(list (channel
+(list 
+     (channel
+      (name 'rde)
+      (url "https://git.sr.ht/~abcdw/rde")
+      (introduction
+       (make-channel-introduction
+	"257cebd587b66e4d865b3537a9a88cccd7107c95"
+	(openpgp-fingerprint
+	"2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
+      (channel
        (name 'nonguix)
        (url "https://gitlab.com/nonguix/nonguix")
        (introduction
